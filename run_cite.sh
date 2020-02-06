cae3aa4e851c2ff3589dbadf6b7abd8a9e7248c5
KDDGRAND <60737534+KDDGRAND@users.noreply.github.com>
2020-02-06 19:29:12 +0800

    Delete run_cite.sh

--- run_cite.sh
+++
@@ -1,1 +0,0 @@
-python train_grand.py --lam 0.7 --tem 0.3 --order 2 --sample 2 --dataset citeseer --input_droprate 0.0 --hidden_droprate 0.2 --hidden 32 --lr 0.01 --patience 200 --seed 100 --dropnode_rate 0.5  --cuda_device 0 

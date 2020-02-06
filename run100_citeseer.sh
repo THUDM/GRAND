ba579b8c8679810457ab84eb124481f3e9b541b9
KDDGRAND <grand_kdd@163.com>
2020-02-06 19:20:04 +0800

    fix sum bugs

---
+++ run100_citeseer.sh
@@ -0,0 +1,5 @@
+mkdir citeseer
+for num in $(seq 0 99)
+do
+	python train_grand.py --lam 0.7 --tem 0.3 --order 2 --sample 2 --dataset citeseer --input_droprate 0.0 --hidden_droprate 0.2 --hidden 32 --lr 0.01 --patience 200 --seed $num --dropnode_rate 0.5  --cuda_device 4 > citeseer/"$num".txt
+done

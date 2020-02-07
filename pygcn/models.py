a7ece6dcf19d7831809c6c3d9e50e66bab16c54a
KDDGRAND <grand_kdd@163.com>
2020-02-07 09:13:01 +0800

    add requirement  Please enter the commit message for your changes. Lines starting

--- pygcn/models.py
+++ pygcn/models.py
@@ -3,37 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.parameter import Parameter
 from torch.nn import init
-from torch_geometric.nn import GATConv
 import numpy as np
 from pygcn.layers import GraphConvolution, MLPLayer
-
-class GAT(torch.nn.Module):
-    def __init__(self, nfeat, nhid, nclass, input_droprate, hidden_droprate, use_bn = False, activation="relu", num_heads=8):
-        super(GAT, self).__init__()
-        self.conv1 = GATConv(
-            nfeat, nhid, heads=num_heads)
-        self.conv2 = GATConv(
-            nhid * num_heads, nclass)
-        self.input_droprate = input_droprate
-        self.hidden_droprate = hidden_droprate
-        self.use_bn = use_bn
-        self.bn1 = nn.BatchNorm1d(nfeat)
-        self.bn2 = nn.BatchNorm1d(nhid * num_heads)
-        assert activation in ["relu", "elu"]
-        self.activation = getattr(F, activation)
-
-    def forward(self, x, edge_index):
-        if self.use_bn:
-            x = self.bn1(x)
-        x = F.dropout(x, self.input_droprate, training=self.training)
-        x = self.activation(self.conv1(x, edge_index))
-        if self.use_bn:
-            x = self.bn2(x)
-        x = F.dropout(x, self.hidden_droprate, training=self.training)
-        x = self.conv2(x, edge_index)
-        return x
-
-
 
 class GCN(nn.Module):
     def __init__(self, nfeat, nhid, nclass, input_droprate, hidden_droprate, use_bn=False):

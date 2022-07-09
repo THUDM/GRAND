7a2fd6e7c3f20ca2c84b06ec1c5dc7f227dbfe2b
wzfhaha <46991380+wzfhaha@users.noreply.github.com>
2022-07-09 08:30:39 +0800

    Update generate_noise_edge.py

--- generate_noise_edge.py
+++ generate_noise_edge.py
@@ -8,7 +8,7 @@ import sys
 dataset_str = 'cora'
 dataset_str = ['cora','citeseer','pubmed']
 
-def generate_noise_edges(dataset_str = 'cora'):
+def generate_noise_edges(dataset_str = 'cora', ratio):
     #for ratio in np.arange(10)/10.:
     with open('data/ind.{}.graph'.format(dataset_str), 'rb') as f:
         if sys.version_info > (3,0):
@@ -18,24 +18,21 @@ def generate_noise_edges(dataset_str = 'cora'):
         G = nx.from_dict_of_lists(graph_f)
         edge_num = G.number_of_edges()
         node_num = G.number_of_nodes()
-        
-        for i in range(10):
-            ratio = (i+1) / 100.
-            noise_edge_num = int(edge_num * ratio)
-            ii = 0 
-            while(True):
-                u,v = random.sample(range(node_num), 2)
-                if not G.has_edge(u,v):
-                    G.add_edge(u,v)
-                    ii += 1
-                if ii >= noise_edge_num:
-                    break
-            print(G.number_of_edges())
-            g_dict_of_list = nx.to_dict_of_lists(G)
-            pkl.dump(g_dict_of_list, open('nsgcn/data/ind.{}.graph.noise.{}'.format(dataset_str, str(ratio)), 'wb'))
+        noise_edge_num = int(edge_num * ratio)
+        ii = 0 
+        while(True):
+            u,v = random.sample(range(node_num), 2)
+            if not G.has_edge(u,v):
+                G.add_edge(u,v)
+                ii += 1
+            if ii >= noise_edge_num:
+                break
+        print(G.number_of_edges())
+        g_dict_of_list = nx.to_dict_of_lists(G)
+        pkl.dump(g_dict_of_list, open('nsgcn/data/ind.{}.graph.noise.{}'.format(dataset_str, str(ratio)), 'wb'))
 
 
-generate_noise_edges('cora')
+generate_noise_edges('cora', 0.1)
 #print(G.edges())
 
 
